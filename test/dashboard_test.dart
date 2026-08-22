6fd7ee0b14a79776e7279c162b77a481a2e4d9bb
Tamer Eldosoky <dosoky.server@gmail.com>
2026-08-22 11:30:16 +0000

    test(dashboard): scroll before asserting lower cards

--- test/dashboard_test.dart
+++ test/dashboard_test.dart
@@ -10,11 +10,14 @@ void main() {
     expect(find.text('تفعيل الفقاعة العائمة'), findsOneWidget);
     expect(find.text('ترجمة نصية'), findsOneWidget);
     expect(find.text('حوار مترجم'), findsOneWidget);
+
+    await tester.scrollUntilVisible(find.text('مستندات وعدسة'), 300);
     expect(find.text('مستندات وعدسة'), findsOneWidget);
     expect(find.text('قصص وإلهام'), findsOneWidget);
 
     await tester.scrollUntilVisible(find.text('ألعاب 3D'), 300);
     expect(find.text('ألعاب 3D'), findsOneWidget);
+    await tester.scrollUntilVisible(find.text('الإعدادات'), 300);
     expect(find.text('الإعدادات'), findsOneWidget);
   });
 }
